1e2d11c5fa30d6c8df068aeac9a584ebf59e86d8
Samuel Delepoulle <samuel.delepoulle@univ-littoral.fr>
2019-08-03 10:36:22 +0200

    config.yml file to choose cheat mode or not

--- src/fr/theobessel/silkspawner/PluginLoader.java
+++ src/fr/theobessel/silkspawner/PluginLoader.java
@@ -1,14 +1,26 @@
 package fr.theobessel.silkspawner;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PluginLoader extends JavaPlugin{
+
+    FileConfiguration config = getConfig();
+
     public static void main(String[] args) {}
+
     @Override
     public void onEnable() {
         System.out.println("[PLUGIN INFO] Plugin started !");
-        getServer().getPluginManager().registerEvents(new SpawnerMining(), this);
+
+        config.addDefault("cheat", false);
+        config.options().copyDefaults(true);
+        saveConfig();
+
+        boolean cheat = config.getBoolean("cheat");
+        getServer().getPluginManager().registerEvents(new SpawnerMining(this, cheat), this);
     }
+
     @Override
     public void onDisable() {
         System.out.println("[PLUGIN INFO] Plugin closed !");
--- src/fr/theobessel/silkspawner/SpawnerMining.java
+++ src/fr/theobessel/silkspawner/SpawnerMining.java
@@ -6,6 +6,7 @@ package fr.theobessel.silkspawner;
 
 import org.bukkit.GameMode;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.block.CreatureSpawner;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -18,8 +19,24 @@ import org.bukkit.block.BlockState;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.enchantments.*;
+import org.bukkit.inventory.meta.tags.CustomItemTagContainer;
+import org.bukkit.inventory.meta.tags.ItemTagType;
+import org.bukkit.plugin.Plugin;
 
 public class SpawnerMining implements Listener {
+
+    private Plugin plugin;
+
+    boolean cheat = false;
+
+    public static String METADATA_TAG = "spawntype";
+
+    public SpawnerMining(Plugin plugin, boolean cheat){
+        this.plugin = plugin;
+        this.cheat = cheat;
+    }
+
+
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
         Block block = event.getBlock();
@@ -27,7 +44,15 @@ public class SpawnerMining implements Listener {
         if (player.getGameMode() != GameMode.CREATIVE) {
             if (block.getType() == Material.SPAWNER) {
                 CreatureSpawner diggedSpawner = (CreatureSpawner) block.getState();
+
+
                 ItemStack handItem = player.getInventory().getItemInMainHand();
+
+                NamespacedKey key = new NamespacedKey(plugin, "type");
+                ItemMeta itemMeta = handItem.getItemMeta();
+                itemMeta.getCustomTagContainer().setCustomTag(key, ItemTagType.STRING, diggedSpawner.getSpawnedType().name());
+                handItem.setItemMeta(itemMeta);
+
 
                 if (handItem == null) return;
                 if (handItem.getType() == Material.DIAMOND_PICKAXE | handItem.getType() == Material.IRON_PICKAXE) {
@@ -37,17 +62,24 @@ public class SpawnerMining implements Listener {
                         BlockState blockState = blockStateMeta.getBlockState();
                         CreatureSpawner spawner = (CreatureSpawner) blockState;
                         spawner.setSpawnedType(diggedSpawner.getSpawnedType());
+
                         spawner.update();
                         blockState.update();
                         blockStateMeta.setBlockState(blockState);
-                        if (enabled) {
-                            blockStateMeta.setDisplayName("§8" + diggedSpawner.getSpawnedType().name());
-                        } else {
+
+
+                        if (cheat) {
                             blockStateMeta.setDisplayName(diggedSpawner.getSpawnedType().name());
                         }
+
                         givedSpawner.setItemMeta(blockStateMeta);
 
+                        ItemMeta meta = handItem.getItemMeta();
+                        //meta.setLore(Arrays.asList(diggedSpawner.getSpawnedType().name()));
+                        givedSpawner.setItemMeta(meta);
+
                         event.setExpToDrop(0);
+
                         player.getInventory().addItem(givedSpawner);
                     }
                 }
@@ -58,24 +90,69 @@ public class SpawnerMining implements Listener {
 
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent event) {
-        if (enabled) {
+
             Block block = event.getBlock();
             Player player = event.getPlayer();
             if (block.getType() == Material.SPAWNER) {
                 CreatureSpawner spawner = (CreatureSpawner) block.getState();
                 ItemStack spawnerItem = player.getInventory().getItemInMainHand();
+
+                NamespacedKey key = new NamespacedKey(plugin, "type");
+                ItemMeta itemMeta = spawnerItem.getItemMeta();
+                CustomItemTagContainer tagContainer = itemMeta.getCustomTagContainer();
+
+
+                /*
+                List<String> lores = new ArrayList<String>();
+                if (itemMeta.getLore() != null){
+                    lores = itemMeta.getLore();
+                }
+
+                System.out.println("lore" + lores);
+                */
+
                 ItemMeta spawnerItemMeta = spawnerItem.getItemMeta();
-                System.out.println(spawner.getSpawnedType().getKey() + " spawner placed");
-                if (enabled) {
+
+                String entityType = "";
+
+
+
+                /*
+                if(cheat) {
+                    System.out.println("cheat");
                     if (spawnerItemMeta.getDisplayName().contains("§8")) {
-                        String entityType = spawnerItemMeta.getDisplayName().replaceAll("§8", "");
+                        entityType = spawnerItemMeta.getDisplayName().replaceAll("§8", "");
                     }
                 } else {
-                    String entityType = spawnerItemMeta.getDisplayName();
+                    System.out.println("pas cheat");
+                    //entityType = spawnerItemMeta.getDisplayName();
                 }
-                spawner.setCreatureTypeByName(entityType);
+                */
+
+                /*
+                System.out.println(entityType);
+
+                if (block.hasMetadata(METADATA_TAG)){
+                    System.out.println( "on devrait spawner des " + block.getMetadata(METADATA_TAG).get(0).value() );
+                }*/
+
+                //spawner.setCreatureTypeByName(entityType);
+                if(tagContainer.hasCustomTag(key , ItemTagType.STRING)) {
+                    String foundValue = tagContainer.getCustomTag(key, ItemTagType.STRING);
+                    System.out.println(foundValue);
+                    spawner.setCreatureTypeByName(foundValue);
+                }
+
+                if(cheat){
+                    entityType = spawnerItemMeta.getDisplayName();
+                    spawner.setCreatureTypeByName(entityType);
+                }
+
                 spawner.update();
+
+                System.out.println(spawner.getSpawnedType().getKey() + " spawner placed");
             }
-        }
+
     }
+
 }
